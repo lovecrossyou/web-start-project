9500a8efdca7f4e19eac91956e65e9298ac48592
JessieJ <85274001@qq.com>
2017-08-14 16:04:02 +0800

    init

--- app.js
+++ app.js
@@ -3,7 +3,10 @@
  */
 var express = require('express');
 var app = express();
+
+app.use('/', require('connect-history-api-fallback')()); // Add
 app.use('/', express.static('public'));
+
 var server = app.listen(2000, function() {
     var port = server.address().port;
     console.log('Open http://localhost:%s', port);
