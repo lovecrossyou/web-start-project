c02ceef6984275775a9518d6f4b1ce670a239c92
JessieJ <85274001@qq.com>
2017-08-15 16:59:59 +0800

    添加界面完善

--- public/index.js
+++ public/index.js
@@ -1,76 +1,109 @@
 /**
  * Created by zhulizhe on 2017/8/14.
  */
-import React from 'react';
-import ReactDOM from 'react-dom';
+var ReactDOM = require("react-dom");
+var React = require("react");
+
+
 import {
-    BrowserRouter as Router,
-    Route,
-    Link
-} from 'react-router-dom';
+    Form, Select, Input, Radio, Button, Upload, Icon,
+} from 'antd';
+const FormItem = Form.Item;
 
-const BasicExample = () => (
-    <Router>
-        <div>
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/topics">Topics</Link></li>
-            </ul>
+class Demo extends React.Component {
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+    normFile = (e) => {
+        console.log('Upload event:', e);
+        if (Array.isArray(e)) {
+            return e;
+        }
+        return e && e.fileList;
+    }
+    render() {
+        const { getFieldDecorator } = this.props.form;
+        const formItemLayout = {
+            labelCol: { span: 6 },
+            wrapperCol: { span: 14 },
+        };
+        return (
+            <Form onSubmit={this.handleSubmit}>
+                <FormItem
+                    {...formItemLayout}
+                    label="投注信息记录"
+                >
+                    <span className="ant-form-text">足彩</span>
+                </FormItem>
+                <FormItem {...formItemLayout} label="主队">
+                    {getFieldDecorator('host', {
+                        rules: [{
+                            required: true,
+                            message: '请输入主队',
+                        }],
+                    })(
+                        <Input placeholder="主队名称" />
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label="客队">
+                    {getFieldDecorator('visitor', {
+                        rules: [{
+                            required: true,
+                            message: '请输入客队信息',
+                        }],
+                    })(
+                        <Input placeholder="客队信息" />
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label="预期">
+                    {getFieldDecorator('expect', {
+                        rules: [{
+                            required: true,
+                            message: '请输入期望结果',
+                        }],
+                    })(
+                        <Input placeholder="预期结果" />
+                    )}
+                </FormItem>
+                <FormItem {...formItemLayout} label="结果">
+                    {getFieldDecorator('result', {
+                        rules: [{
+                            required: false,
+                            message: '请输入开奖结果',
+                        }],
+                    })(
+                        <Input placeholder="开奖结果" />
+                    )}
+                </FormItem>
+                <FormItem
+                    {...formItemLayout}
+                    label="图表数据"
+                >
+                    {getFieldDecorator('upload', {
+                        valuePropName: 'fileList',
+                        getValueFromEvent: this.normFile,
+                    })(
+                        <Upload name="logo" action="/upload.do" listType="picture">
+                            <Button>
+                                <Icon type="upload" /> Click to upload
+                            </Button>
+                        </Upload>
+                    )}
+                </FormItem>
+                <FormItem
+                    wrapperCol={{ span: 12, offset: 6 }}
+                >
+                    <Button type="primary" htmlType="submit">Submit</Button>
+                </FormItem>
+            </Form>
+        );
+    }
+}
 
-            <hr/>
-
-            <Route exact path="/" component={Home}/>
-            <Route path="/about" component={About}/>
-            <Route path="/topics" component={Topics}/>
-        </div>
-    </Router>
-)
-
-const Home = () => (
-    <div>
-        <h2>Home</h2>
-    </div>
-)
-
-const About = () => (
-    <div>
-        <h2>About</h2>
-    </div>
-)
-
-const Topics = ({ match }) => (
-    <div>
-        <h2>Topics</h2>
-        <ul>
-            <li>
-                <Link to={`${match.url}/rendering`}>
-                    Rendering with React
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/components`}>
-                    Components
-                </Link>
-            </li>
-            <li>
-                <Link to={`${match.url}/props-v-state`}>
-                    Props v. State
-                </Link>
-            </li>
-        </ul>
-
-        <Route path={`${match.url}/:topicId`} component={Topic}/>
-        <Route exact path={match.url} render={() => (
-            <h3>Please select a topic.</h3>
-        )}/>
-    </div>
-)
-
-const Topic = ({ match }) => (
-    <div>
-        <h3>{match.params.topicId}</h3>
-    </div>
-)
-
-ReactDOM.render(<BasicExample/>, document.getElementById('main'));
+const WrappedDemo = Form.create()(Demo);
+ReactDOM.render(<WrappedDemo/>, document.getElementById('main'));
